a639f640df772566f1c2dadd9588e461f877b9c0
dsanmart <diegosancarrion@gmail.com>
2022-12-19 23:19:18 +0100

    Add star icon

--- frontend/src/components/recipes/RecipeList.jsx
+++ frontend/src/components/recipes/RecipeList.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import RecipeForm from './RecipeForm';
 import "./RecipeForm.css";
 import 'bootstrap/dist/css/bootstrap.min.css'; // import bootstrap styles
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link> // import font awesome styles
 import {
     Button,
     Table,
@@ -113,7 +114,7 @@ return (
                         <td>{recipe.recipe_name}</td>
                         <td>{recipe.ingredients}</td>
                         <td>{recipe.steps}</td>
-                        <td>{recipe.rating}</td>
+                        <td><i className={recipe.favorite ? 'fa fa-star' : 'fa fa-star-o'}></i></td>
                         <td>{recipe.favorite}</td>
                         <td>
             <Button color="primary" onClick={() => handleEditRecipe(recipe)}>Edit</Button>{' '}
