194fb6c8b0e132b1ab117e96466c9e6780620a7a
Diego Sanmartin <92187824+dsanmart@users.noreply.github.com>
2022-12-20 11:13:01 +0100

    Update RecipeList.jsx

--- frontend/src/components/recipes/RecipeList.jsx
+++ frontend/src/components/recipes/RecipeList.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import RecipeForm from './RecipeForm';
 import "./RecipeForm.css";
-import 'bootstrap/dist/css/bootstrap.min.css'; // import bootstrap styles
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link> // import font awesome styles
 import {
     Button,
     Table,
@@ -11,6 +9,8 @@ import {
     ModalHeader,
     ModalBody,
 } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'; // import bootstrap styles
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link> // import font awesome styles
 
 const RecipeList = () => {
     const [recipes, setRecipes] = useState([]);
